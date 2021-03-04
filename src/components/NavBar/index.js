492a22562146dc93052f470bf2d73b6c27d226ef
Jae Kim <jaeshinkim321@gmail.com>
2021-03-04 11:50:39 -0800

    basic navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import "./styles.css"
 
 export default function NavBar() {
     return (
-        <Navbar expand="lg" className="NavBar">
-            <Navbar.Brand href="/">Home</Navbar.Brand>
-        </Navbar>
+            <Navbar expand="lg" className="NavBar">
+                <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Navbar.Brand href="/login">Log In</Navbar.Brand>
+                <Navbar.Brand href="/login">Sign In</Navbar.Brand>
+            </Navbar>
     )
 }
 
