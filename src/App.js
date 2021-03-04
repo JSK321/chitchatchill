b9e21181463f81fcbec2d6438531adfcf07cb528
Jae Kim <jaeshinkim321@gmail.com>
2021-03-04 12:16:54 -0800

    basic chat box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { useState, useEffect } from 'react'
-import API from './utils/API'
+// import API from './utils/API'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './components/NavBar'
+import ChatBox from './components/ChatBox'
 
 function App() {
   return (
     <Router>
       <NavBar />
+
+      <Switch>
+        <Route exact path="/">
+          <ChatBox />
+        </Route>
+      </Switch>
     </Router>
   );
 }
