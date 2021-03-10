e50e6d85997675e8db3e2cb1fbd8cbd87575a42c
Jae Kim <jaeshinkim321@gmail.com>
2021-03-09 17:26:28 -0800

    edit message

--- src/components/ChatBox/index.js
+++ src/components/ChatBox/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Card, Form, ListGroup } from 'react-bootstrap'
+import { Card, Form, ListGroup, Button, ButtonGroup } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import "./styles.css"
 
 export default function ChatBox(props) {
+
     return (
         <Card className="chatBox">
             <Card.Header className="cardHeader">{props.roomName}</Card.Header>
@@ -13,19 +16,69 @@ export default function ChatBox(props) {
                     props.messages.map(data => (
                         <ListGroup.Item
                             key={data.id}
-                            id={data.id}
+                            // id={`message${data.id}`}
                             className="chatMessage"
                         >
+                            {/* User Account Name */}
                             <strong>{props.accountName}</strong>
+
+                            {/* TimeStamp */}
                             <span className="createdAtTime">
                                 {" " + data.createdAt.slice(0, -8) + "at" + " " + data.createdAt.slice(11)}
                             </span>
-                            <br></br>
-                            {data.message}
+
+                            {/* Edit Icon */}
+                            <FontAwesomeIcon
+                                icon={faEdit}
+                                className="editIcon"
+                                onClick={props.handleEditMessage}
+                                id={data.id}
+                            />
+
+                            {/* Chat Message */}
+                            <p
+                                id={`message${data.id}`}
+                            >
+                                {data.message}
+                            </p>
+
+                            {/* Edit Message Form */}
+                            <Form
+                                id={`editMessage${data.id}`}
+                                className="editForm"
+                                hidden="true"
+                            >
+                                <Form.Control
+                                    as="textarea"
+                                    rows={3}
+                                    className="editChatMessage"
+                                    placeholder="Edit message"
+                                    name="editMessage"
+                                    value={props.editMessage}
+                                    onChange={props.handleEditInputChange}
+                                />
+                                <Button
+                                    className="saveBtn"
+                                    onClick={props.handleSaveBtn}
+                                    size="sm"
+                                >
+                                    Save
+                                </Button>
+                                <Button
+                                    className="cancelBtn"
+                                    onClick={props.handleCancelBtn}
+                                    size="sm"
+                                >
+                                    Cancel
+                                </Button>
+                            </Form>
+
                         </ListGroup.Item>
                     ))
                 }
             </ListGroup>
+
+            {/* Send Message Form */}
             <Form
                 onSubmit={props.handleSendMessage}
                 className="sendMessageInput"
@@ -39,6 +92,7 @@ export default function ChatBox(props) {
                     autoComplete="off"
                 />
             </Form>
+
         </Card>
     );
 };
--- src/pages/ChatPage/index.js
+++ src/pages/ChatPage/index.js
@@ -1,38 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import API from '../../utils/API';
-import ChatBox from '../../components/ChatBox'
+import ChatBox from '../../components/ChatBox';
+
 
 export default function ChatPage() {
+    // useState of chatroom
     const [chatRoomState, setChatRoomState] = useState({
         roomName: "",
         id: ""
     });
-
+    // useState of user info
     const [userState, setUserState] = useState({
         accountName: "",
         id: "",
         token: "",
         profileImage: ""
     });
-
+    // useState of message input
     const [chatState, setChatState] = useState({
         message: "",
         ChatRoomId: ""
     });
-
+    // useState of edit message input
+    const [editChatState, setEditChatState] = useState({
+        editMessage: "",
+        ChatRoomId: ""
+    });
+    // useState of all messages in chatroom
     const [messageState, setMessageState] = useState({
         messages: []
-    })
-
+    });
+    // param of id on chatroom
     const { id } = useParams();
-    const { chatRoom } = useParams();
 
     useEffect(() => {
         userData();
         findChatRoom();
     }, []);
-
+    // Function to retrieve user data
     function userData() {
         const token = localStorage.getItem("token");
         if (localStorage.getItem('token') !== null) {
@@ -47,10 +53,14 @@ export default function ChatPage() {
                     ...chatState,
                     ChatRoomId: id
                 });
+                setEditChatState({
+                    ...editChatState,
+                    ChatRoomId: id
+                })
             });
         };
     };
-
+    // Function to find chatroom data
     function findChatRoom() {
         API.getOneChatRoom(id).then(data => {
             setChatRoomState({
@@ -58,23 +68,23 @@ export default function ChatPage() {
                 id: data.id
             });
         });
-        getAllMessages()
+        getAllMessages();
     };
-
+    // Function to retrieve all messages in chatroom
     function getAllMessages() {
         API.getAllMessages(id).then(data => {
             setMessageState({
                 messages: data
-            })
-            updateScroll()
-        })
-    }
-
+            });
+            updateScroll();
+        });
+    };
+    // Function to update scroll on page
     function updateScroll() {
         let chatHistory = document.getElementsByClassName("chatDisplay");
-        chatHistory[0].scrollTop = chatHistory[0].scrollHeight
-    }
-
+        chatHistory[0].scrollTop = chatHistory[0].scrollHeight;
+    };
+    // Function to handle input change on message
     const handleInputChange = event => {
         const { name, value } = event.target
         setChatState({
@@ -82,28 +92,100 @@ export default function ChatPage() {
             [name]: value
         });
     };
-
-    const handleSendMessage = event => {
+    // Function to handle edit message state
+    const handleEditInputChange = event => {
+        const { name, value } = event.target
+        setEditChatState({
+            ...editChatState,
+            [name]: value
+        })
+    }
+    // Function to handle edit icon
+    const handleEditMessage = event => {
         event.preventDefault();
-        API.createMessage(userState.token, chatState).then(after => {
-            getAllMessages()
-            updateScroll()
-            setChatState({
-                ...chatState,
-                message: ""
+        let id = event.currentTarget.id
+        let message = document.getElementById(`message${id}`)
+        let editForm = document.getElementById(`editMessage${id}`)
+        let openForm = document.getElementsByClassName('editForm')
+        let chatHistory = document.getElementsByClassName("chatDisplay")
+        // Check to see if any edit forms are open
+        for (let i = 0; i < openForm.length; i++) {
+            if (openForm[i].hidden === false) {
+                openForm[i].hidden = true
+                openForm[i].previousSibling.hidden = false
+            }
+        }
+
+        if (editForm.hidden === true) {
+            editForm.hidden = false
+            message.hidden = true
+        }
+        // console.log(chatHistory[0].scrollTop)
+        if (chatHistory[0].scrollTop > 0) {
+            updateScroll();
+        }
+
+        API.getOneMessage(id).then(data => {
+            setEditChatState({
+                ...editChatState,
+                editMessage: data.message
             })
         })
     };
+    // Function to handle save button
+    const handleSaveBtn = event => {
+        event.preventDefault();
+        let editParent = event.target.parentNode;
+        let id = editParent.id.slice(11);
+        let message = document.getElementById(`message${id}`);
+        API.updateOneMessage(userState.token, id, editChatState.editMessage).then(after => {
+            getAllMessages();
+        })
+        if (editParent.hidden === false) {
+            editParent.hidden = true
+            message.hidden = false
+        };
+    }
+    // Function to handle cancel button
+    const handleCancelBtn = event => {
+        event.preventDefault();
+        let editParent = event.target.parentNode;
+        let id = editParent.id.slice(11);
+        let message = document.getElementById(`message${id}`);
+        if (editParent.hidden === false) {
+            editParent.hidden = true
+            message.hidden = false
+        };
+    };
+    // Function to post new message in chat room
+    const handleSendMessage = event => {
+        event.preventDefault();
+        API.createMessage(userState.token, chatState).then(after => {
+            getAllMessages();
+            updateScroll();
+            setChatState({
+                ...chatState,
+                message: ""
+            });
+        });
+    };
 
-    
+
     return (
         <ChatBox
+            // States
             roomName={chatRoomState.roomName}
             message={chatState.message}
             messages={messageState.messages}
+            editMessage={editChatState.editMessage}
             accountName={userState.accountName}
+            // Handle Events
             handleInputChange={handleInputChange}
+            handleEditMessage={handleEditMessage}
+            handleSaveBtn={handleSaveBtn}
+            handleCancelBtn={handleCancelBtn}
             handleSendMessage={handleSendMessage}
+            handleEditInputChange={handleEditInputChange}
         />
     );
 };
